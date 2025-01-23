f538bc2ac37b2c0bd5f5476fba7168c1ae729117
Sergio <94061941+sergio-mugen-power@users.noreply.github.com>
2025-01-23 13:43:01 +0100

    Actualizados parametros de scrapper para arreglar error: Audi A8 argo

--- scraper_wallapop_con_parametros_json_Audi_A6.py
+++ scraper_wallapop_con_parametros_json_Audi_A6.py
@@ -265,7 +265,7 @@ min_horsepower = 1
 base_directory = 'parameters_scrapper'
 for subdir, _, files in os.walk(base_directory):
     for file in files:
-        if file.endswith('.json') and file.startswith('Audi_A6'):
+        if file.endswith('.json') and file.startswith('Audi_A8'):
             file_path = os.path.join(subdir, file)
             with open(file_path, 'r', encoding='utf-8') as json_file:
                 data = json.load(json_file)
