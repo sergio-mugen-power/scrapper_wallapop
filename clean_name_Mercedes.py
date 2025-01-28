3865c08071c89c770137ad54735ee79c85ad129d
Sergio <94061941+sergio-mugen-power@users.noreply.github.com>
2025-01-28 17:36:50 +0100

    Limpiado nombres de Mercedes para el Scrapper

--- clean_name_Mercedes.py
+++ clean_name_Mercedes.py
@@ -5,11 +5,38 @@ import json
 directory = 'Transformed_data/Mercedes-Benz'
 
 # Define the words to be removed from the 'cleaned_name' field
-words_to_remove = ["Coupé","Roadster"]
+words_to_remove = ["Coupé","Roadster", "Extralarga", "Cabrio", "Berlina", "All-Terrain","Largo", "Stirling Moss", "Compacta", "Larga", "Extralarga", "Compacta", "Extralargo", "Compacto", "Corto"]
 # Function to clean the 'cleaned_name' field
 def clean_name(name):
-    if name.startswith("AMG"):
-        return name
+    if name.startswith("AMG"): return name
+    if name.startswith("Citan"): name = "Citan"
+    if name.startswith("CL"): name = "CL Coupe"
+    if name.startswith("Clase C"): name = "Clase C"
+    if name.startswith("Clase E"): name = "Clase E"
+    if name.startswith("Clase S"): name = "Clase S"
+    if name.startswith("Clase V"): name = "Clase V"
+    if name.startswith("Clase X"): name = "Clase X"
+    if name.startswith("Clase G"): name = "Clase G"
+    if name.startswith("Clase A"): name = "Clase A"
+    if name.startswith("Clase B"): name = "Clase B"
+    if name.startswith("Clase GLA"): name = "GLA"
+    if name.startswith("Clase GLB"): name = "GLB"
+    if name.startswith("Clase GLC"): name = "GLC"
+    if name.startswith("Clase GLE"): name = "GLE"
+    if name.startswith("Clase GLS"): name = "GLS"
+    if name.startswith("Clase SLC"): name = "GLC"
+    if name.startswith("Clase SL"): name = "SL"
+    if name.startswith("Clase SLK"): name = "SLK"
+    if name.startswith("Clase SLR"): name = "SLR"
+    if name.startswith("Clase SLK"): name = "SLK"
+    if name.startswith("SLS"): name = "SLS"
+    if name.startswith("EQA"): name = "EQA (H243)"
+    if name.startswith("EQB"): name = "EQB (X243)"
+    if name.startswith("EQC"): name = "EQC (N293)"
+    if name.startswith("EQE"): name = "EQE (V297)"
+    if name.startswith("EQE SUV"): name = "EQE SUV (Z294)"
+    if name.startswith("EQS"): name = "EQS (V297)"
+
     for word in words_to_remove:
         # Caso especial dentro del bucle
         if name == "AMG GT 4 puertas Coupé": name = "AMG GT 4-Door Coupé"
